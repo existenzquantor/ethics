85479caccb7c62c9130ec7acbf0c4595574c84a5
Felix Lindner <lindner@informatik.uni-freiburg.de>
2019-09-18 17:02:56 +0200

    update: setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(name='ethics',
-      version='0.18.8',
+      version='0.19',
       description = ("A python toolbox for ethical reasoning."),
       author='Felix Lindner',
       author_email='info@hera-project.com',
       url='http://www.hera-project.com',
-      py_modules=['ethics.plan_semantics', 'ethics.plan_principles' , 'ethics.language', 'ethics.cam_semantics', 'ethics.cam_principles', 'ethics.tools', 'ethics.verbalizer', 'ethics.explanations', 'ethics.solver', 'ethics.primes', 'ethics.argumentation'],
+      py_modules=['ethics.plans.semantics', 'ethics.plans.principles' , 'ethics.language', 'ethics.cam.semantics', 'ethics.cam.principles', 'ethics.tools', 'ethics.verbalizer', 'ethics.explanations', 'ethics.solver', 'ethics.primes', 'ethics.argumentation'],
       install_requires=['PyYAML', 'BinPy', 'pyeda']
       )
