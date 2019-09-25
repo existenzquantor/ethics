4faa1b15f400e0fdbdab5e0cb468aecd0ced3a07
Felix Lindner <felix@existenzquantor.info>
2019-09-25 18:43:22 +0200

    explanations umgebaut

--- ethics/explanations.py
+++ ethics/explanations.py
@@ -1,14 +1,59 @@
 from ethics.language import *
-from ethics.primes import *
+from ethics.solver import *
+
+
+def find_all_models(formula):
+    return list(map(set, smt_all_models(formula)))
+
+
+def hitting_sets_gde(sets):
+    sets = sorted(sets, key=len)
+    hitting_sets = [set()]
+    for current_set in sets:
+        new_hitting_sets = list(hitting_sets)
+        for hitting_set in hitting_sets:
+            # Check if hitting_set hits current_set
+            if hitting_set.intersection(current_set) == set():
+                new_hitting_sets.remove(hitting_set)
+                # no hit
+                for element in current_set:
+                    candidate = hitting_set.union({element})
+
+                    is_valid = True
+                    for hs in new_hitting_sets:
+                        if hs.issubset(candidate):
+                            is_valid = False
+                            break
+                    if is_valid:
+                        new_hitting_sets.append(candidate)
+
+        hitting_sets = list(new_hitting_sets)
+
+    return hitting_sets
+
+def remove_trivial_clauses(clauses):
+    r = []
+    for c in clauses:
+        neg_c = {n.getNegation() for n in c}
+        if len(c & neg_c) == 0:
+            r.append(c)
+    return r
+
+def compute_primes(formula):
+    models = find_all_models(formula)
+    prime_implicates = remove_trivial_clauses(hitting_sets_gde(models))
+    prime_implicants = remove_trivial_clauses(hitting_sets_gde(prime_implicates))
+    return prime_implicants, prime_implicates
+
 
 def generate_reasons(model, principle, *args):
     perm = principle.permissible()
 
     # Compute prime implicants and prime implicates
     if perm:
-        cants, cates = PrimeCompilator(principle.buildConjunction(), use_mhs_only =True).compile()
+        cants, cates = compute_primes(principle.buildConjunction().nnf())
     else:
-        cants, cates = PrimeCompilator(Not(principle.buildConjunction()).nnf(), use_mhs_only=True).compile()
+        cants, cates = compute_primes(Not(principle.buildConjunction()).nnf())
 
     # Sufficient reasons from prime implicants
     suff = [Formula.makeConjunction(c) for c in cants if model.models(Formula.makeConjunction(c))]
@@ -48,3 +93,5 @@ def generate_inus_reasons(reasons):
                 inus.append(Formula.makeDisjunction(rn_check))
                 break
     return inus
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 setup(name='ethics',
-      version='0.19.4',
+      version='0.19.5',
       description = ("A python toolbox for ethical reasoning."),
       author='Felix Lindner',
       author_email='info@hera-project.com',
       url='http://www.hera-project.com',
-      py_modules=['ethics.plans.semantics', 'ethics.plans.principles' , 'ethics.language', 'ethics.cam.semantics', 'ethics.cam.principles', 'ethics.tools', 'ethics.verbalizer', 'ethics.explanations', 'ethics.solver', 'ethics.primes'],
-      install_requires=['PyYAML', 'BinPy', 'pyeda']
+      py_modules=['ethics.plans.semantics', 'ethics.plans.principles' , 'ethics.language', 'ethics.cam.semantics', 'ethics.cam.principles', 'ethics.tools', 'ethics.verbalizer', 'ethics.explanations', 'ethics.solver'],
+      install_requires=['PyYAML', 'pyeda']
       )
