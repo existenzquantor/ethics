1d74e48bf161c3fedb7fb2692d79dcfa43d3301c
Felix Lindner <fl@kant.local>
2019-12-11 20:28:09 +0100

    Robot & Frank Domain

---
+++ test/test-robot-frank.py
@@ -0,0 +1,57 @@
+from ethics.plans.semantics import Situation
+from ethics.plans.planner import Planner, MoralPlanner
+from ethics.plans.principles import KantianHumanity, DoNoHarm, DoNoInstrumentalHarm, Utilitarianism, Deontology, GoalDeontology, DoubleEffectPrinciple, AvoidAnyHarm, AvoidAvoidableHarm
+
+
+print("*"*50)
+print("JSON")
+print("*"*50)
+sit = Situation("cases/plans/robot_and_frank.yaml")
+
+print("Intial State: "+ str(sit.init))
+print("Plan: "+ str(sit.plan))
+print("Final State: "+ str(sit.get_all_consequences()))
+
+perm = sit.evaluate(Deontology)
+print("Deontology: ", perm)
+print("Deontology Explanation:", sit.explain(Deontology))
+
+perm = sit.evaluate(GoalDeontology)
+print("GoalDeontology: ", perm)
+print("GoalDeontology Explanation:", sit.explain(GoalDeontology))
+
+perm = sit.evaluate(KantianHumanity)
+print("Kantian: ", perm)
+print("Kantian Explanation:", sit.explain(KantianHumanity))
+
+perm = sit.evaluate(DoNoHarm)
+print("DoNoHarm: ", perm)
+print("DoNoHarm Explanation:", sit.explain(DoNoHarm))
+
+perm = sit.evaluate(AvoidAnyHarm)
+print("AvoidAnyHarm: ", perm)
+print("AvoidAnyHarm Explanation:", sit.explain(AvoidAnyHarm))
+
+perm = sit.evaluate(DoNoInstrumentalHarm)
+print("DoNoInstrumentalHarm: ", perm)
+print("DoNoInstrumentalHarm Explanation:", sit.explain(DoNoInstrumentalHarm))
+
+perm = sit.evaluate(Utilitarianism)
+print("Utilitarianism: ", perm)
+print("Utilitarianism Explanation:", sit.explain(Utilitarianism))
+
+perm = sit.evaluate(DoubleEffectPrinciple)
+print("DoubleEffectPrinciple: ", perm)
+print("DoubleEffectPrinciple Explanation:", sit.explain(DoubleEffectPrinciple))
+
+perm = sit.evaluate(AvoidAvoidableHarm)
+print("AvoidAvoidableHarm: ", perm)
+print("AvoidAvoidableHarm Explanation:", sit.explain(AvoidAvoidableHarm))
+
+
+planner = Planner(sit)
+p = planner.generate_plan()
+if p:
+    print("Planned: ", p.plan)
+else:
+    print("No Plan found")
